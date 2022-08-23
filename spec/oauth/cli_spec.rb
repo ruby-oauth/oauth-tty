0580f942bf91e5ac54bf1662bea231e0a7a251cf
Peter Boling <peter.boling@gmail.com>
2022-08-23 20:13:57 +0700

    Initial commit

---
+++ spec/oauth/cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Oauth::Cli do
+  it "has a version number" do
+    expect(Oauth::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
