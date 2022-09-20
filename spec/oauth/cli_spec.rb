1280596d670101651f75bc061db56b5edd9d9590
Peter Boling <peter.boling@gmail.com>
2022-09-20 12:40:50 +0700

    🔥 Remove bogus test

--- spec/oauth/cli_spec.rb
+++ spec/oauth/cli_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe OAuth::TTY do
   it "has a version number" do
     expect(OAuth::TTY::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
